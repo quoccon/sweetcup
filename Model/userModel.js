551f98f53e07c03607edb79ca08c342d1c41ccd0
quoccon <quocnvph23753@fpt.edu.vn>
2023-08-15 22:01:13 +0700

    mongo

---
+++ Model/userModel.js
@@ -0,0 +1,18 @@
+var db = require("./db");
+const userSchema= new db.mongoose.Schema(
+    {
+        username:{type:String, require:true},
+        email:{type:String, require:true},
+        password:{type:String, require:true},
+        role:{type:Number, require:true},
+
+    },
+    {
+        collection:'user'
+    }
+
+);
+
+let userModel= db.mongoose.model('userModel', userSchema);
+
+module.exports={userModel};
