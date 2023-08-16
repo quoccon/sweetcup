e4e783e3ea1cb8a3d3080a86f10ea1c516ce68c8
quoccon <quocnvph23753@fpt.edu.vn>
2023-08-16 20:48:50 +0700

    abc

--- Model/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/Server_duan')
-.catch((err)=>{
-    console.log('Loi ket noi CSDL');
-    console.log(err);
-})
-
-module.exports = {mongoose};
-
---
+++ src/controller/product.controller.js
@@ -0,0 +1,2 @@
+// const myMD = require("../Model/productModel");
+
