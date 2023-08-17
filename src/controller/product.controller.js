88130e0f4d5445660bea500912e2ce8c52a8197e
quoccon <quocnvph23753@fpt.edu.vn>
2023-08-17 21:24:08 +0700

    abc

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -1,3 +1,4 @@
+// const { method } = require('bluebird');
 const myMD = require('../Model/productModel');
 
 
@@ -7,3 +8,51 @@ exports.getListProduct = async (req, res, next) => {
     console.log(listSp);
     res.render('../views/products/product.ejs',{listSp:listSp});
 }
+
+var fs = require('fs');
+exports.add= async(req, res, next)=>{
+    let msg='';
+    console.log(req.file, req.body);
+    let listCat = await myMD.catModel.find();
+    if(req.method=='POST'){
+
+        fs.rename(req.file.path,
+            './public/templates/'+ req.file.originalname,
+            (err)=>{
+               if(err)
+                   console.log(err);
+               else{
+                   // không có lỗi, tạo url, bỏ chữ public/
+               console.log("Url: http://localhost:3000/templates/" +req.file.originalname );
+               }
+            }) 
+
+            let objSP= new myMD.spModel();
+            objSP.name= req.body.name;
+            objSP.price= req.body.price;
+            objSP.description= req.body.description;
+            objSP.image= "http://localhost:3000/templates/"+req.file.originalname ;
+            objSP.id_cat= req.body.category;
+            
+            try {
+                let new_sp= await objSP.save();
+                console.log(new_sp);
+                msg="Đã thêm thành công";
+                
+            } catch (error) {
+                msg="Lỗi :"+error.message;
+                console.log(error);
+            }
+        }
+
+        
+    
+
+
+    res.render('products/addProduct', {msg:msg, listCat:listCat});
+};
+
+
+// exports.deleteProduct = () => {
+
+// }
