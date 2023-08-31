0b332ecca642cfc75895624622939269e388fe43
dunghmph18450 <you@example.com>
2023-08-31 21:40:59 +0700

    update

--- src/Model/userModel.js
+++ src/Model/userModel.js
@@ -3,6 +3,7 @@ const userSchema= new db.mongoose.Schema(
     {
         username:{type:String, require:true},
         email:{type:String, require:true},
+        phone:{type:String, require:true},
         password:{type:String, require:true},
         avata:{type:String,required:false}
         
