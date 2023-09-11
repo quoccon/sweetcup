978d13b8b98689784a10dac080479a6e9d76b7dc
quoccon <quocnvph23753@fpt.edu.vn>
2023-09-11 13:46:20 +0700

    fix lỗi hiện ảnh

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -1,130 +1,99 @@
-// const { method } = require('bluebird');
 const path = require("path");
 const myMD = require("../Model/productModel");
+const fs = require("fs");
 
-//Hiển thị danh sách sản phẩm
-
+// Hiển thị danh sách sản phẩm
 exports.getListProduct = async (req, res, next) => {
-  var listSp = await myMD.productModel.find();
-
-  console.log(listSp);
-  res.render("../views/products/product.ejs", { listSp: listSp });
+  try {
+    const listSp = await myMD.productModel.find();
+    console.log(listSp);
+    res.render("../views/products/product.ejs", { listSp: listSp });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Đã xảy ra lỗi trong quá trình lấy danh sách sản phẩm.");
+  }
 };
 
-
-
-//Thêm mới sản phẩm
-var fs = require("fs");
+// Thêm mới sản phẩm
 exports.add = async (req, res, next) => {
-    console.log("Thêm Sản Phẩm");
+  console.log("Thêm Sản Phẩm");
   let msg = "";
-  
-  // let listCat = await myMD.catModel.find();
-  if (req.method == "POST") {
-    console.log(req.file);
-    if (req.file != null) {
-      const destinationPath = path.join(__dirname, "../public/templates");
-      const tempFilePath = req.file.path;
 
-      fs.rename(
-        tempFilePath,
-        path.join(destinationPath, req.file.originalname),
-        (err) => {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(
-              "Url: http://localhost:8080/templates/" +
-                req.file.originalname +
-                "success"
-            );
-          }
-        }
-      );
-    }else{
-      console.log("deo duoc");
-    }
-    var objSP = new myMD.productModel();
-    objSP.nameproduct = req.body.nameproduct;
-    objSP.price = req.body.price;
-    objSP.description = req.body.description;
-    if (req.body.image != null) {
-      objSP.image = "http://localhost:8080/templates/" + req.file.originalname;
-    }else{
-      console.log("dsdsd");
-    }
-    objSP.id_cat= req.body.category;
+  if (req.method === "POST") {
+    try {
+      let image = "";
+      if (req.file != null) {
+        const destinationPath = path.join(__dirname, "../public/templates");
+        const tempFilePath = req.file.path;
+        const originalName = req.file.originalname;
 
-    
-      try {
-        let new_sp = await objSP.save();
-        console.log(new_sp);
-        msg = "Đã thêm thành công";
-      } catch (error) {
-        msg = "Lỗi :" + error.message;
-        console.log(error);
+        fs.renameSync(tempFilePath, path.join(destinationPath, originalName));
+        console.log("Url: http://localhost:8080/templates/" + originalName + "success");
+        
+        image = "/templates/" + originalName;
       }
-    
+
+      const objSP = new myMD.productModel({
+        nameproduct: req.body.nameproduct,
+        price: req.body.price,
+        description: req.body.description,
+        image: image,
+        id_cat: req.body.category,
+      });
+
+      const new_sp = await objSP.save();
+      console.log(new_sp);
+      msg = "Đã thêm thành công";
+    } catch (error) {
+      msg = "Lỗi: " + error.message;
+      console.error(error);
+    }
   }
 
   res.redirect("/product");
 };
 
-
-//Xóa sản phẩm
+// Xóa sản phẩm
 exports.deleteProduct = async (req, res, next) => {
-  var idPro = req.params.idSp;
+  const idPro = req.params.idSp;
   console.log(idPro);
 
   try {
     await myMD.productModel.deleteOne({ _id: idPro });
     res.redirect("/product");
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    res.status(500).send("Đã xảy ra lỗi trong quá trình xóa sản phẩm.");
   }
 };
 
-
-//Sửa sản phẩm
-exports.editPro = async (req,res,next)=>{
-  var idSp = req.params.idSp;
+// Sửa sản phẩm
+exports.editPro = async (req, res, next) => {
+  const idSp = req.params.idSp;
   console.log(idSp);
-  if (req.method == 'POST') {
-    const destinationPath = path.join(
-      __dirname,
-      "../public/templates"
-    );
-    const tempFilePath = req.file.path;
 
-    fs.rename(
-      tempFilePath,
-      path.join(destinationPath, req.file.originalname),
-      (err) => {
-        if (err) {
-          console.log(err);
-        } else
-          console.log(
-            "Url: http://localhost:8080/templates/" +
-              req.file.originalname +
-              "Chuyển OKe"
-          );
-      }
-    );
-    var objPro = new myMD.productModel();
-    objPro.nameproduct = req.body.nameproduct;
-    objPro.price = req.body.price;
-    objPro.description = req.body.description;
-    objPro.image = "http://localhost:8080/templates/" + req.file.originalname;
-    objPro._id= idSp;
-
+  if (req.method === 'POST') {
     try {
-      await myMD.productModel.updateOne({_id: idSp},objPro)
-      console.log('Update thành công')
-      res.redirect('/product')
+      const destinationPath = path.join(__dirname, "../public/templates");
+      const tempFilePath = req.file.path;
+      const originalName = req.file.originalname;
+
+      fs.renameSync(tempFilePath, path.join(destinationPath, originalName));
+      console.log("Url: http://localhost:8080/templates/" + originalName + "Chuyển OKe");
+
+      const updatedProduct = {
+        nameproduct: req.body.nameproduct,
+        price: req.body.price,
+        description: req.body.description,
+        image: "/templates/" + originalName,
+      };
+
+      await myMD.productModel.updateOne({ _id: idSp }, updatedProduct);
+      console.log('Update thành công');
+      res.redirect('/product');
     } catch (error) {
-      console.log(error)
+      console.error(error);
+      res.status(500).send("Đã xảy ra lỗi trong quá trình cập nhật sản phẩm.");
     }
-
   }
-
-}
+};
