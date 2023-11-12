c0e706186c29960499ecf5cf61c860b6c624546c
dunghmph18450 <you@example.com>
2023-11-12 16:04:09 +0700

    haha

--- src/controller/API/use.api.js
+++ src/controller/API/use.api.js
@@ -266,9 +266,9 @@ exports.getAddress = async (req, res) => {
   const idu = req.query.userId;
   console.log(idu)
   try {
-    const findUser = await myMDA.AddressModel.find({id_user: idu})
-    
-    res.json(findUser)
+    const findAdress = await myMDA.AddressModel.find({id_user: idu})
+    objReturn.info_user = findAdress
+    res.json(findAdress)
   } catch (error) {
   console.log(error)  
   res.status(500).json({ error: 'Có lỗi xảy ra trong quá trình populate wishlist.' });  
